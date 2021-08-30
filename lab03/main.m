ea230df463d82e3276762f052d9201acc45756d9
DvoryadkinaDV <ddvoradkina@gmail.com>
2021-08-30 20:26:43 +0300

    Initial Commit

---
+++ lab03/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lab03
+//
+//  Created by Диана on 30.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
